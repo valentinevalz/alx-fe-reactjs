368738d373006001387e2dba3f114bf85a39d61b
valentinevalz <odedeyivaz@gmail.com>
2025-07-31 23:46:13 +0100

    ✅ Refactor: Fully replaced prop drilling with Context API for user data

--- alx-react-app-props/src/UserInfo.jsx
+++ alx-react-app-props/src/UserInfo.jsx
@@ -1,10 +1,8 @@
-// alx-react-app-props/src/UserInfo.jsx
-import React from 'react';
-import UserDetails from './UserDetails'; // Import the next component
+// src/UserInfo.jsx
+import UserDetails from './UserDetails';
 
-// This component also used to get userData as a prop, but now it doesn't need to!
 function UserInfo() {
-  return <UserDetails />; // Just passes along the message
+  return <UserDetails />;
 }
 
 export default UserInfo;
