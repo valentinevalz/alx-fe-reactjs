63946f3c4b6b5060e2f924697da0ae7298870790
valentinevalz <odedeyivaz@gmail.com>
2025-08-01 04:04:26 +0100

    ✅ Refactor: Used Context API to eliminate prop drilling across components

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -3,10 +3,7 @@ import ProfilePage from './ProfilePage';
 import UserContext from './UserContext';
 
 function App() {
-  const userData = {
-    name: "Jane Doe",
-    email: "jane.doe@example.com"
-  };
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
     <UserContext.Provider value={userData}>
---
+++ alx-react-app-props/src/UserProfile.jsx
@@ -0,0 +1,8 @@
+// src/UserProfile.jsx
+import UserInfo from './UserInfo';
+
+function UserProfile() {
+  return <UserInfo />;
+}
+
+export default UserProfile;
