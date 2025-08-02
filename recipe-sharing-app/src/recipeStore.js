f29abbb933fe09915b2e964e563bcbf238fd46fd
valentinevalz <odedeyivaz@gmail.com>
2025-08-02 09:48:49 +0100

    ✅ Init: Set up Recipe Sharing App with Zustand state management

---
+++ recipe-sharing-app/src/recipeStore.js
@@ -0,0 +1,8 @@
+// src/recipeStore.js
+import { create } from 'zustand';
+
+export const useRecipeStore = create((set) => ({
+  recipes: [],
+  addRecipe: (recipe) =>
+    set((state) => ({ recipes: [...state.recipes, recipe] })),
+}));
---
+++ recipe-sharing-app/src/store/recipeStore.js
@@ -0,0 +1,9 @@
+import { create } from 'zustand';
+
+export const useRecipeStore = create((set) => ({
+  recipes: [],
+  addRecipe: (recipe) =>
+    set((state) => ({
+      recipes: [...state.recipes, recipe],
+    })),
+}));
